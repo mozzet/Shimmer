80fe2465c4e04f06b8ab494dff63dab7bcf8e727
Jo Sesang <ss.cho@mozzet.com>
2020-04-02 17:46:34 +0900

    카르타고 전용 워크스페이스 제작 작업

---
+++ Shimmer/Shimmer.h
@@ -0,0 +1,19 @@
+//
+//  Shimmer.h
+//  Shimmer
+//
+//  Created by MOZZET PC on 2020/04/02.
+//  Copyright © 2020 Noondate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shimmer.
+FOUNDATION_EXPORT double ShimmerVersionNumber;
+
+//! Project version string for Shimmer.
+FOUNDATION_EXPORT const unsigned char ShimmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shimmer/PublicHeader.h>
+
+
