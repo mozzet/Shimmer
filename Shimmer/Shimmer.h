c8a7a908a2d8d3aaa6801faeadf1cb6594bd07f6
Jo Sesang <ss.cho@mozzet.com>
2020-04-03 16:51:57 +0900

    Shimmer 버그 해결

---
+++ Shimmer/Shimmer.h
@@ -0,0 +1,23 @@
+//
+//  Shimmer.h
+//  Shimmer
+//
+//  Created by MOZZET PC on 2020/04/03.
+//  Copyright © 2020 Noondate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shimmer.
+FOUNDATION_EXPORT double ShimmerVersionNumber;
+
+//! Project version string for Shimmer.
+FOUNDATION_EXPORT const unsigned char ShimmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shimmer/PublicHeader.h>
+
+
+#import <Shimmer/FBShimmering.h>
+#import <Shimmer/FBShimmeringLayer.h>
+#import <Shimmer/FBShimmeringView.h>
+
